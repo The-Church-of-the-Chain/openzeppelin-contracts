ea2b8e67bdbeb0cc15577ad1ce8e8bf4bd6a4681
Yaxoria <yaxoria@chain.church>
2020-11-29 15:12:30 -0700

    Renaming things

--- contracts/wip/libraries/security/structs/RoleData.sol
+++ contracts/wip/libraries/security/structs/RoleData.sol
@@ -24,52 +24,52 @@ library RoleData {
         mapping(bytes32 => Role) roles;
     }
     
-    function registerRole( Role storage self_, bytes32 role_, address rootAccount_ ) internal {
-        self_.admin = role_;
-        self_.members.add( rootAccount_ );
-        self_.approved[rootAccount_] = true;
+    function registerRole( Role storage self, bytes32 role, address rootAccount ) internal {
+        self.admin = role;
+        self.members.add( rootAccount );
+        self.approved[rootAccount] = true;
     }
     
     // TODO: This shit is retarded. Why can I not just access the (add etc.) function directly in the Datastore contract even if it is public in the AddressSet library?
-    function grantRole( Role storage self_, address member_ ) internal {
-        self_.members.add( member_ );
+    function assignRole( Role storage self, address member ) internal {
+        self.members.add( member );
     }
     
-    function removeRole( Role storage self_, address member_ ) internal {
-        self_.members.remove( member_ );
+    function removeRole( Role storage self, address member ) internal {
+        self.members.remove( member );
     }
     
-    function addRestrictedRole( Role storage self_, bytes32 role_ ) internal {
-        self_.restrictedRoles.add( role_ );
+    function addRestrictedRole( Role storage self, bytes32 role ) internal {
+        self.restrictedRoles.add( role );
     }
     
-    function removeRestrictedRole( Role storage self_, bytes32 role_ ) internal {
-        self_.restrictedRoles.remove( role_ );
+    function removeRestrictedRole( Role storage self, bytes32 role ) internal {
+        self.restrictedRoles.remove( role );
     }
     
-    function memberCount( Role storage self_ ) internal view returns ( uint256 ) {
-        return self_.members.length();
+    function memberCount( Role storage self ) internal view returns ( uint256 ) {
+        return self.members.length();
     }
     
-    function restrictedCount( Role storage self_ ) internal view returns ( uint256 ) {
-        return self_.restrictedRoles.length();
+    function restrictedCount( Role storage self ) internal view returns ( uint256 ) {
+        return self.restrictedRoles.length();
     }
     
-    function getMember( Role storage self_, uint256 index_ ) internal view returns ( address ) {
-        return self_.members.at( index_ );
+    function getMember( Role storage self, uint256 index ) internal view returns ( address ) {
+        return self.members.at( index );
     }
     
-    function getRestrictedRole( Role storage self_, uint256 index_ ) internal view returns ( bytes32 ) {
-        return self_.restrictedRoles.at( index_ );
+    function getRestrictedRole( Role storage self, uint256 index ) internal view returns ( bytes32 ) {
+        return self.restrictedRoles.at( index );
     }
     
-    function isMember( Role storage self_, address member_ ) internal view returns ( bool ) {
-        return self_.members.contains( member_ );
+    function isMember( Role storage self, address member ) internal view returns ( bool ) {
+        return self.members.contains( member );
     }
     
-    function createRole( Role storage self_, bytes32 admin_, bytes32 approver_ ) internal {
-        self_.admin = admin_;
-        self_.approver = approver_;
+    function createRole( Role storage self, bytes32 admin_, bytes32 approver_ ) internal {
+        self.admin = admin_;
+        self.approver = approver_;
     }
 
 }
